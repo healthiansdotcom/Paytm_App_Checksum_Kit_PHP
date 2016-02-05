3646a9aa5a4607b7a9f6e2e619845505701ccefa
vidisha-goel <vidisha.goel@paytm.com>
2016-02-05 13:43:53 +0530

    updated

--- lib/config_paytm.php
+++ lib/config_paytm.php
@@ -1,10 +1,4 @@
 <?php
-/*
-- Change the value of PAYTM_MERCHANT_KEY constant with details received from Paytm.
-
-*/
-
+//Change the value of PAYTM_MERCHANT_KEY constant with details received from Paytm.
 define('PAYTM_MERCHANT_KEY', 'XXXXXXXXXXXXXXX'); 
-
-
 ?>
