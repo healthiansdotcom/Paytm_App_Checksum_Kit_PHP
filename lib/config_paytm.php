a024e85bda41695bc3210bbaec405a5e467b5516
vidisha <vidisha.goel@paytm.com>
2016-02-05 13:42:07 +0530

    App Checksum Kit Submitted

--- lib/config_paytm.php
+++ lib/config_paytm.php
@@ -1,25 +1,10 @@
 <?php
 /*
-
-- Use PAYTM_ENVIRONMENT as 'PROD' if you wanted to do transaction in production environment else 'TEST' for doing transaction in testing environment.
 - Change the value of PAYTM_MERCHANT_KEY constant with details received from Paytm.
-- Change the value of PAYTM_MERCHANT_MID constant with details received from Paytm.
-- Change the value of PAYTM_MERCHANT_WEBSITE constant with details received from Paytm.
-- Above details will be different for testing and production environment.
 
 */
-define('PAYTM_ENVIRONMENT', 'PROD'); // TEST
-define('PAYTM_MERCHANT_KEY', 'XXXXXXXXXXXXXXX'); //Change this constant's value with Merchant key received from Paytm
-define('PAYTM_MERCHANT_MID', 'WorldP64425807474247'); //Change this constant's value with MID (Merchant ID) received from Paytm
-define('PAYTM_MERCHANT_WEBSITE', 'worldpressplg'); //Change this constant's value with Website name received from Paytm
 
-$PAYTM_DOMAIN = "pguat.paytm.com";
-if (PAYTM_ENVIRONMENT == 'PROD') {
-	$PAYTM_DOMAIN = 'secure.paytm.in';
-}
+define('PAYTM_MERCHANT_KEY', 'XXXXXXXXXXXXXXX'); 
 
-define('PAYTM_REFUND_URL', 'https://'.$PAYTM_DOMAIN.'/oltp/HANDLER_INTERNAL/REFUND');
-define('PAYTM_STATUS_QUERY_URL', 'https://'.$PAYTM_DOMAIN.'/oltp/HANDLER_INTERNAL/TXNSTATUS');
-define('PAYTM_TXN_URL', 'https://'.$PAYTM_DOMAIN.'/oltp-web/processTransaction');
 
 ?>
