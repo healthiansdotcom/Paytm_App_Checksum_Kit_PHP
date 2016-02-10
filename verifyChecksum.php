68adbf1cdabbe19ee0de5e92dbd3c971710af135
vidisha <vidisha.goel@paytm.com>
2016-02-10 15:41:14 +0530

    App Checksum Kit Submitted

--- verifyChecksum.php
+++ verifyChecksum.php
@@ -18,21 +18,17 @@ $paytmChecksum = isset($_POST["CHECKSUMHASH"]) ? $_POST["CHECKSUMHASH"] : ""; //
 //Verify all parameters received from Paytm pg to your application. Like MID received from paytm pg is same as your application’s MID, TXN_AMOUNT and ORDER_ID are same as what was sent by you to Paytm PG for initiating transaction etc.
 $isValidChecksum = verifychecksum_e($paramList, PAYTM_MERCHANT_KEY, $paytmChecksum); //will return TRUE or FALSE string.
 
-if ($isValidChecksum===TRUE)
-	$return_array["IS_CHECKSUM_VALID"] = "Y";
-else
-	$return_array["IS_CHECKSUM_VALID"] = "N";
+// if ($isValidChecksum===TRUE)
+// 	$return_array["IS_CHECKSUM_VALID"] = "Y";
+// else
+// 	$return_array["IS_CHECKSUM_VALID"] = "N";
 
-//$return_array["IS_CHECKSUM_VALID"] = $isValidChecksum ? "Y" : "N";
-$return_array["TXNTYPE"] = "";
-$return_array["REFUNDAMT"] = "";
+$return_array["IS_CHECKSUM_VALID"] = $isValidChecksum ? "Y" : "N";
+//$return_array["TXNTYPE"] = "";
+//$return_array["REFUNDAMT"] = "";
 unset($return_array["CHECKSUMHASH"]);
 
 $encoded_json = htmlentities(json_encode($return_array));
-
-//============  Sample json response passed to SDK after verifying checksum  ==================================
-
-//    { "TXNID": "4203335",    "BANKTXNID": "",    "ORDERID": "ORDER1409950517",    "TXNAMOUNT": "1",    "STATUS": TXN_SUCCESS",    "TXNTYPE": "",    "CURRENCY": "INR",    "GATEWAYNAME": "ICICI",    "RESPCODE": "01",    "RESPMSG": "Txn Successfull.",    "BANKNAME": "HDFC",    MID": "robosf49909586699899",    "PAYMENTMODE": "CC",    "REFUNDAMT": "",    "TXNDATE": "2013­04­19 14:35:50.775483",    "IS_CHECKSUM_VALID": "Y"}
 
 ?>
 
